b26db34e550245a0d0cb8814495a4ef02bdab69f
Sergey Babiak <spbabiak@gmail.com>
2026-01-29 21:03:44 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+$('.mobile div').on('click', () => {
+    $('.mobile div').toggleClass('active');
+    $('.mobile nav').toggleClass('open');
+    $('.mobile nav ul').toggleClass('show');
+});
+
+for (let a = 1; a <= $(".mobile ul li").length; a++){
+    $(".mobile ul li:nth-child("+ a +")").css("animation-delay", "."+ (a+1) +"s");   
+}
+
+$('.feature').on('click', function() {
+     if($(this).hasClass('hidden')) {
+        $(this).removeClass('hidden');
+        $(this).find('p').show();
+    }else {
+        $(this).addClass('hidden');
+        $(this).find('p').hide();
+    }
+});
